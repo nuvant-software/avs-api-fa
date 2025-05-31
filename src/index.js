3536e53426f1800aa57907f81099743ad6392aba
arsenharutjunjan <nuvant.software@gmail.com>
2025-05-31 22:40:29 +0200

    github

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions_1 = require("@azure/functions");
-const fetch_items_1 = require("./functions/fetch-items");
-functions_1.app.http('fetch-items', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
-    handler: fetch_items_1.fetchItems
-});
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-// No imports needed for v4 programming model
-import "./fetch-items";
